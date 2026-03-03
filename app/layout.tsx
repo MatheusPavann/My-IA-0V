abb5f4148b7d145811edfa963e437e1f7472dfa9
Matheus Pavan Borges <131879971+MatheusPavann@users.noreply.github.com>
2026-03-03 17:42:14 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { ThemeProvider } from 'next-themes'
-import DeployBanner from '../components/deploy-banner'
+
 import './globals.css'
 
 const geistSans = Geist({
